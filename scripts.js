8d6ed6220958d21c2cb889548a68e88e47155bfd
Jack Emond <JackEmond@outlook.com>
2021-04-27 22:26:36 -0400

    Added a better design and a grid size slider

--- scripts.js
+++ scripts.js
@@ -1,28 +1,58 @@
 
-const rows = 16;
-const columns = 16;
 
 const container = document.querySelector("#container");
-container.style.cssText = 'grid-template-columns: repeat(' + columns + ',auto); grid-template-rows: repeat('+rows+', auto)';
 
-numberofSquareDivsRequired = rows * columns;
-currentNumberOfSquareDivs = 0;
 
-while(currentNumberOfSquareDivs < numberofSquareDivsRequired)
-{
-    const squareDiv = document.createElement('div');
-    container.appendChild(squareDiv);
-    squareDiv.className = 'squareDiv';
-    currentNumberOfSquareDivs += 1;
+
+numberOfRowsandColumns();
+
+function numberOfRowsandColumns(columns = 60){
+    var rows = columns;
+  container.innerHTML = '';
+  container.style.cssText = 'grid-template-columns: repeat(' + columns + ',auto); grid-template-rows: repeat('+rows+', auto)';
 }
 
-const buttons = document.querySelectorAll('.squareDiv');
-buttons.forEach((div) => {
+numberofSquareDivs();
 
-    // and for each one we add a 'click' listener
-    div.addEventListener('mouseout', () => {
-      div.style.cssText = "background-color: blue;";
+function numberofSquareDivs(columns = 60){
+  var rows = columns;
+  numberofSquareDivsRequired = rows * columns;
+  currentNumberOfSquareDivs = 0;
+
+  while(currentNumberOfSquareDivs < numberofSquareDivsRequired)
+  {
+      const squareDiv = document.createElement('div');
+      container.appendChild(squareDiv);
+      squareDiv.className = 'squareDiv';
+      currentNumberOfSquareDivs += 1;
+  }
+}
+
+addEventListener();
+
+function addEventListener(){
+  const squareDivs = document.querySelectorAll('.squareDiv');
+  squareDivs.forEach((div) => {
+      div.addEventListener('mouseout', () => {
+      div.style.cssText = "background-color: black;";
     });
   });
+}
 
+
+
+const slider = document.querySelector("#slider");
+slider.addEventListener('change', refreshStats);
+
+function refreshStats(){
+  numberOfRowsandColumns(this.value);
+  numberofSquareDivs(this.value);
+  addEventListener();
+
+}
+
+
+
+var x = document.querySelector("#slider").value;
+console.log(x);
 
